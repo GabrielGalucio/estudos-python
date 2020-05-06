63d5d4362fde6b1db3c6860ffb4c3090d439484e
GabrielGalucio <gmlgalucio@gmail.com>
2020-05-06 00:23:26 -0400

    021_commit

--- Py_DSA_LV02/005_loops_aninhados.py
+++ Py_DSA_LV02/005_loops_aninhados.py
@@ -30,3 +30,29 @@ for i in lista03:
     contador = contador + 1
 
 print(contador) 
+
+# Contando o número de colunas
+lista_matriz = [[0,0,1,0],[1,2,0,1],[2,4,0,1],[3,0,0,0]]
+primeira_linha = lista_matriz[0]
+contador_lista = 0
+
+for i in primeira_linha:
+    contador_lista = contador_lista + 1
+
+print(contador_lista)
+
+# Pesquisando itens em uma lista
+lista04 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for i in lista04:
+    if i == 5:
+        print("Achou")
+
+# Podemos usar o loop for com dicionários. Podemos listar os valores de um dicionário
+dicionario01 = {"chave01":1, "cahve02":2, "chave03":3, "chave04":4}
+for i in dicionario01:
+    print(i)
+
+# Podemos retornar a chave e o valor do dicionario vamos utilizar o metodo chamado
+# dict.items() que é um metodo do dicionario para retornar itens
+for chave, valor in dict.items(dicionario01):
+    print(chave, valor)
