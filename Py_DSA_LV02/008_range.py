b0606bf9622d4261684b55fbfd404fca4e723691
GabrielGalucio <gmlgalucio@gmail.com>
2020-05-09 15:34:08 -0400

    021_commit

---
+++ Py_DSA_LV02/008_range.py
@@ -0,0 +1,8 @@
+# A função range nos permite criar uma lista de números, com intervaos especificos
+# range(começar, parar, passo)
+# range(inicio, fim, condição)
+
+# começar/inicio : é o número que inicia a sequencia
+# parar/ fim: é o número que encerra a sequencia (não é incluso na sequencia) 
+# passo/condição : diferença entre cada número da sequencia
+
