670bd16ade02c4f859b607ed55353f6f73a1b071
GabrielGalucio <gmlgalucio@gmail.com>
2020-05-09 15:50:12 -0400

    022_commit

--- Py_DSA_LV02/008_range.py
+++ Py_DSA_LV02/008_range.py
@@ -6,3 +6,30 @@
 # parar/ fim: é o número que encerra a sequencia (não é incluso na sequencia) 
 # passo/condição : diferença entre cada número da sequencia
 
+# Impressão simples
+for i in range(10, 20, 3):
+    print(i)
+
+
+# Impressão sem condição
+for i in range(0, 10):
+    print(i)
+
+# Impressão com númers negativos
+for i in range(0, -20, -2):
+    print(i)
+
+# Impressão com uma lista
+lista = ["café", "morango", "açaí", "farinha", "peixe"] # Declara um lista
+lista_tamanho = len(lista)                              # Aloca em uma variavel o tamanho da lista com a função len()
+for i in range(0, lista_tamanho):                       # Cria um for com range 0(inicio) e o tamanho da lista (com len vira um numero)
+    print(lista[i])                                     # Imprime a lista na posição em que i está, ou seja cada item a lista
+
+# Verrificando o tamanho da lista, observe
+print(lista_tamanho)
+print(lista[0])
+# print(lista_tamanho[0])
+
+# Tudo em python é um objeto, inclusive range
+
+print(type(range(0,10)))
