6459bf5c15e9fbf1820b8af2b602f294be56270f
GabrielGalucio <gmlgalucio@gmail.com>
2020-05-10 18:54:03 -0400

    024_commit

---
+++ Py_DSA_LV02/009_metodos.py
@@ -0,0 +1,4 @@
+# Metodos são "funções" incorporadas ao objeto -> sua sintaxe .nomemetodo() pode ser anexada
+# ao objeto que estamos manipulando, com ela podemos manipular ainda melhor esses objetos. Os
+# metodos permitem eecutar ações especificas nos objetos e também podem levar parametros assim
+# como as funções com a sintaxe objeto.metodo(arg1, arg2 ....)
