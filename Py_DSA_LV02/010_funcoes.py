0e6ee03f46018982ea0b3c457bae92278fd1ff4c
GabrielGalucio <gmlgalucio@gmail.com>
2020-05-11 23:14:50 -0400

    024_commit

---
+++ Py_DSA_LV02/010_funcoes.py
@@ -0,0 +1,10 @@
+# Função: é um dispositivo que agrupa um conjunto de código para que eles possam ser
+# executadas mais de uma vez. Funções também permitem especificar com parametros que
+# podem servir como entradas paras as funções.
+# 
+# Em um nível mais fundamental, a construção de funções nos permite reutilizar o codi
+# go, sem ter que screve-lo novamente. Nas aulas de string, utilizamos a função len()
+# para obter o comprimento de uma string. Com funções, escrevemos o código uma única
+# vez e repetimos a mesma instrução através de chamadas, quantos vees forem necesaarias
+
+ 
