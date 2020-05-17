c27be05126f4473daf6a14a4c2e53a747882a800
GabrielGalucio <gmlgalucio@gmail.com>
2020-05-17 13:09:03 -0400

    030_commit

---
+++ Py_DSA_LV03/001_Leitura_Arquivos.py
@@ -0,0 +1,16 @@
+"""
+Definindo arquivos
+
+Python usa objetos de arquivo para interagir com arquivos em seu computador. Esses objetos de
+arquivo podem ser qualquer tipo de arquivo como um arquivo de aúdio, um arquivo de texto, e-m
+mails, documentos do Excel etc. Será necessário instalar bibliotecas ou modulos para execitar.
+
+A linguagem (assim como outras) tem metodos built-in (internos) para manipulação de arquivos txt
+
+open() Usado para abrir um arquivo
+raad() Usado para ler um arquivo
+write() Usado para gravação no arquivo
+seek() Retorna para o inicio do arquivo
+readlines() Retorna a lista de linhas do arquivo
+close() Fecha o arquivo
+"""
