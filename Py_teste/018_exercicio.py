644676c0f0679f05211425289bdff2401ce83469
GabrielGalucio <gmlgalucio@gmail.com>
2020-05-23 23:54:31 -0400

    038_commit

--- Py_teste/018_exercicio.py
+++ Py_teste/018_exercicio.py
@@ -2,6 +2,11 @@
 # de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do 
 # arquivo usando este link (em minutos).
 
-tam_mb = int(input("Digite aqui o tamanho do dowload em MB: "))
-tam_mbps = int(input("Digite aqui a velocidade da internet em Mbps: "))
+tamanho_mb = int(input("Digite aqui o tamanho do dowload em MB: "))
+velocidade = int(input("Digite aqui a velocidade da internet em Mbps: "))
 
+tamanhoBits = tamanho_mb * 1024 * 1024 * 8
+tempoSegundos = tamanhoBits / (velocidade * 1024 * 1024)
+tempoMinutos = tempoSegundos / 60
+
+print("Tempo aproximado de dowload: ", tempoMinutos, tempoSegundos)
